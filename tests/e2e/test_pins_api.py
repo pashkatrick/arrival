33b6db67e6495ff0208eedc24120163245caac8c
pudins <pudins@pudinss-Mac-mini.local>
2022-06-22 22:59:47 +0300

    [init] init some struct

---
+++ tests/e2e/test_pins_api.py
@@ -0,0 +1,31 @@
+import allure
+
+
+@allure.feature('')
+@allure.suite('')
+class TestPinsGetMethods:
+
+    @allure.description('')
+    def test1(self):
+        with allure.step(f''):
+            pass
+
+    @allure.description('')
+    def test2(self):
+        with allure.step(f''):
+            pass
+
+
+@allure.feature('')
+@allure.suite('')
+class TestPinsPostMethods:
+
+    @allure.description('')
+    def test1(self):
+        with allure.step(f''):
+            pass
+
+    @allure.description('')
+    def test2(self):
+        with allure.step(f''):
+            pass
---
+++ tests/e2e/test_signals_api.py
@@ -0,0 +1,16 @@
+import allure
+
+
+@allure.feature('')
+@allure.suite('')
+class TestSignalsGetMethods:
+
+    @allure.description('')
+    def test1(self):
+        with allure.step(f''):
+            pass
+
+    @allure.description('')
+    def test2(self):
+        with allure.step(f''):
+            pass
