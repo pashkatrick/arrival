3062810c9e217dfe27ed52e994f19a45072970b1
pashkatrick <coolbatmanarrow@gmail.com>
2022-06-23 12:18:00 +0300

    [init] smoke cases + configs + restclient

---
+++ clients/restclient.py
@@ -0,0 +1,21 @@
+import requests as req
+
+
+class RestClient:
+
+    def __init__(self, host='localhost', port=8099):
+        self.host = f'http://{host}:{port}'
+        self.body = dict(
+            headers={'custom-header': 'example'}
+        )
+
+    def post(self, path: str, data: dict) -> req.Response:
+        self.body['url'] = self.host + path
+        self.body['data'] = data
+        resp = req.get(**self.body)
+        return resp
+
+    def get(self, path: str) -> req.Response:
+        self.body['url'] = self.host + path
+        resp = req.get(**self.body)
+        return resp
---
+++ tests/e2e/smoke/test_pins_api.py
@@ -0,0 +1,49 @@
+import allure
+import pytest
+from hamcrest import *
+
+
+@allure.feature('Pins')
+@allure.suite('Get Requests')
+class TestPinsGetMethods:
+
+    @pytest.fixture(autouse=True)
+    def init(self, restclient):
+        self.client = restclient
+
+    @allure.description('')
+    def test_pins(self):
+        response = self.client.get('/api/pins')
+        assert_that(response.status_code, equal_to(
+            200), '/pins endpooint error')
+        assert_that(response.status_code, greater_than(0), 'empty /pins data')
+
+    @allure.description('')
+    @pytest.mark.parametrize('pin_id', [1, 5])
+    def test_pin_by_id(self, pin_id):
+        response = self.client.get(f'/api/pins/{pin_id}')
+        assert_that(response.status_code, equal_to(
+            200), f'/pins/{pin_id} endpooint error')
+
+
+@allure.feature('Pins')
+@allure.suite('Post Requests')
+class TestPinsPostMethods:
+
+    @pytest.fixture(autouse=True)
+    def init(self, restclient):
+        self.client = restclient
+
+    @pytest.mark.skip(reason='empty data')
+    @allure.description('')
+    def test_update_pins_bulk(self, body):
+        response = self.client.post(f'/api/pins/update_pin', data=body)
+        assert_that(response.status_code, equal_to(200), 'example error')
+
+    @pytest.mark.skip(reason='empty data')
+    @allure.description('')
+    def test_update_pin(self, pin_id, body):
+        response = self.client.post(
+            f'/api/pins/{pin_id}/update_pin', data=body)
+        assert_that(response.status_code, equal_to(
+            200), f'example error for pin {pin_id}')
